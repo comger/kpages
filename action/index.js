e8bc226827b6fe0d4fcebf3c00dac4894d8a3d66
comger <comger@gmail.com>
2012-07-14 11:23:57 +0800

    ..

--- action/index.js
+++ action/index.js
@@ -1,13 +1,22 @@
-Uti = require('./core/utility').Uti;
+Uti = require('../core/server').Uti;
 
 var index = {
     home:function(req, res){
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.write("this's home");
         res.end();
+    },
+    about:function(req, res){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write("this's about");
+        res.end();
     }
 }
 
-Uti.setRouter(index.home, '/home');
 
-exports.module = index;
+var handlers = [];
+Uti.setRouter(handlers,index.home, '/home');
+Uti.setRouter(handlers,index.about, '/about');
+
+
+exports.module = handlers;
