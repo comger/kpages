ce5c0d9536d20142b292516f85ba71d22f7898a2
comger <kpages@163.com>
2013-06-28 17:50:48 +0800

    add settings

---
+++ kpages/settings.py
@@ -0,0 +1,47 @@
+# -*- coding:utf-8 -*-
+
+"""
+    系统配置文件
+
+    不建议修改本文件，应用设置应该保存在 /settings.py 应用配置文件中。
+    应用配置会覆盖同名的系统配置。
+
+"""
+
+# 服务器监听端口
+PORT = 8088
+
+
+# SESSION 过期时间(秒)
+SESSION_EXPIRE = 60 * 10
+
+# 是否启动调试模式
+DEBUG = True
+
+# 是否启动压缩
+GZIP = True
+
+# 静态目录名
+STATIC_DIR_NAME = "static"
+
+# 模板目录名
+TEMPLATE_DIR_NAME = "template"
+
+
+# COOKIE 加密
+COOKIE_SECRET = "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo="
+
+# COOKIE 安全
+XSRF_COOKIES = False
+
+
+# 缓存服务器
+CACHE_SERVERS = "localhost"
+
+# 数据库服务器
+DB_HOST = "localhost"
+
+# 数据库名
+DB_NAME = "test"
+
+
