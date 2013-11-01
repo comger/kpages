c0ac33fe59dac62c50224787b11467d86c21d2f8
comger <comger@gmail.com>
2013-11-01 14:23:20 +0800

    mod demo

--- demos/web/service.py
+++ demos/web/service.py
@@ -39,12 +39,14 @@ def _show_info(**kwargs):
 
 def _get_opt():
     parser = OptionParser("%prog [options]", version="%prog v0.9")
-    parser.add_option("--host", dest="host", default=None,
-                      help="Redis server address.")
-    parser.add_option("--channel", dest="channel", default=None,
-                      help="Subscribe channel name.")
-    parser.add_option("--debug", dest="debug", action="store_true",
-                      default=None, help="Debug mode.")
+    parser.add_option("--config", dest="config",
+                      default='setting.py', help="config for app")
+    parser.add_option("--host", dest="host",
+                      default=None, help="Redis server address.")
+    parser.add_option("--channel", dest="channel",
+                      default=None, help="Subscribe channel name.")
+    parser.add_option("--debug", dest="debug",
+                      action="store_true", default=None, help="Debug mode.")
     parser.add_option("--ndebug", dest="debug",
                       action="store_false", help="No Debug mode.")
 
@@ -53,13 +55,15 @@ def _get_opt():
 
 def main():
     opts, args = _get_opt()
-    reflesh_config()
+    reflesh_config(opts.config)
     if opts.debug is not None:
         __conf__.DEBUG = opts.debug
-
-    Service(
-        host=opts.host, channel=opts.channel, callback=_show_info).run()
+    
+    Service(host=__conf__.CACHE_HOST or opts.host, channel=__conf__.SERVICE_CHANNEL or opts.channel,
+            callback=_show_info).run()
 
 
 if __name__ == "__main__":
     main()
+
+# vim: ts=4 sw=4 sts=4 expandtab
