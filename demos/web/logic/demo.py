dae4f7b8804e33636b2cb521330cc595076e0dce
comger <comger@gmail.com>
2014-02-12 20:26:36 +0800

    ..

--- demos/web/logic/demo.py
+++ demos/web/logic/demo.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
-
-from kpages import srvcmd
+import tornado.gen as gen
+from kpages import srvcmd,get_context
 
 @srvcmd('demofun')
 def demofun(data):
@@ -11,3 +11,10 @@ def demofun(data):
 @srvcmd('demofun')
 def demofun1(data):
     print '2',data
+
+
+@gen.coroutine
+def async_get():
+    db = yield get_context().get_motor()
+    coll = yield db['test']
+    return coll.find()
--- demos/web/restful/index.py
+++ demos/web/restful/index.py
@@ -3,10 +3,11 @@
     index action demo
     author comger@gmail.com
 """
+import motor
 import tornado
 
 from tornado import gen
-from kpages import url, ContextHandler, LogicContext, get_context, service_async
+from kpages import url, ContextHandler, LogicContext, get_context, service_async,mongo_conv
 
 
 @url(r"/")
@@ -14,3 +15,30 @@ class IndexHandler(ContextHandler,tornado.web.RequestHandler):
     def get(self):
         self.write('Hello world!')
 
+
+@url(r"/json")
+class JsonHandler(ContextHandler,tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        coll = get_context().get_async_mongo()['account']
+        cursor = coll.find()
+        motor.Op(cursor.to_list,callback=self.end)
+    
+    def end(self,rs):
+        self.write(dict(data=mongo_conv(list(rs))))
+        self.finish()
+
+@gen.engine
+def get_motor(callback=None):
+    mc = motor.MotorClient(host=__conf__.DB_HOST).open_sync()
+    coll = mc.migrant.account
+    coll.find({},callback=callback) 
+
+@url(r"/json1")
+class Json1Handler(ContextHandler,tornado.web.RequestHandler):
+    def get(self):
+        coll = get_context().get_mongoclient(name='migrant')['account']
+        rs = coll.find()
+        
+        self.write(dict(data=mongo_conv(list(rs))))
+        self.finish()
