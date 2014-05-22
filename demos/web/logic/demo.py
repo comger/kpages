f719c55ec538fb2fad3dbcf85eb0e60c1462aea2
comger <comger@gmail.com>
2014-05-22 14:16:39 +0800

    remove demo bugs

--- demos/web/logic/demo.py
+++ demos/web/logic/demo.py
@@ -1,5 +1,4 @@
 # -*- coding:utf-8 -*-
-import tornado.gen as gen
 from kpages import srvcmd,get_context
 
 @srvcmd('demofun')
@@ -13,8 +12,3 @@ def demofun1(data):
     print '2',data
 
 
-@gen.coroutine
-def async_get():
-    db = yield get_context().get_motor()
-    coll = yield db['test']
-    return coll.find()
