fb1163c535882847a78740e88b5052d60fba38ec
zhangxj <zhangxj@smartbow.net>
2015-04-27 16:46:03 +0800

    fix service

--- kpages/service.py
+++ kpages/service.py
@@ -12,12 +12,11 @@
         2011-08-03  + srvcmd 装饰器
         2011-08-04  * 取消 Service callback，改为自动查找 service function。
                     + 增加 _send_pack, _unpack。
-        2011-08-07  * 将 Consumer 从 Service 中分离。便于单元测试使用。
+        2011-08-07  * 将 PSConsumer 从 Service 中分离。便于单元测试使用。
         2011-08-27  + 新增 service_async 函数。
         2011-08-28  * 重构 Pack 为一个独立类。
         2011-08-29  * 取消 Timeout。
 """
-import uuid
 import time
 import copy
 import datetime
@@ -84,15 +83,6 @@ class DateTimeEncoder(json.JSONEncoder):
 class Pack(object):
 
     @staticmethod
-    def send_pack(mq, channel, cmd, data):
-        data['uuid'] = str(uuid.uuid1())
-        cmd_key = '{}_{}'.format(cmd, data['uuid'])
-        mq.lpush(__conf__.SERVICE_LISTKEY, cmd_key)        
-        
-        pack = dumps(dict(cmd=cmd, data=data), cls=DateTimeEncoder)
-        mq.publish(channel, pack)
-
-    @staticmethod
     def unpack(data):
         try:
             pack = loads(data)
@@ -102,11 +92,18 @@ class Pack(object):
 
     @classmethod
     def async_send_pack(cls, cmd, data, channel=None):
-        cls.send_pack(get_context(
-        ).get_redis(), channel or __conf__.SERVICE_CHANNEL, cmd, data)
+        r = get_context().get_redis()
+        pack = dumps(dict(cmd=cmd, data=data))
+        r.publish(channel or __conf__.SERVICE_CHANNEL, pack)
+
+    @classmethod
+    def async_queue(cls, cmd, data, channel=None):
+        r = get_context().get_redis()
+        pack = dumps(dict(cmd=cmd, data=data))
+        r.rpush(channel or __conf__.SERVICE_CHANNEL, pack)
 
 
-class Consumer(object):
+class PSConsumer(object):
     def __init__(self, channel, host="localhost"):
         self._channel = channel
         self._host = host
@@ -115,13 +112,15 @@ class Consumer(object):
         self._redis = Redis(host=self._host) 
         self._pubsub = self._redis.pubsub()
         self._pubsub.subscribe(self._channel)
-        self._listen = None
 
     def consume(self):
-        if not self._listen:
-            self._listen = self._pubsub.listen()
+        try:
+            data = self._pubsub.listen().next()["data"]
+            return Pack.unpack(data)
+        except Exception as e:
+            self._pubsub.subscribe(self._channel)
+            return None, None
 
-        return Pack.unpack(self._listen.next()["data"])
 
     def close(self):
         if hasattr(self, "_redis"):
@@ -142,11 +141,9 @@ class Service(object):
         self._host = host or "localhost"
         self._channel = channel or __conf__.SERVICE_CHANNEL
         self._processes = __conf__.DEBUG and 1 or cpu_count()
-        #self._processes = 1
 
-        self._consumer = Consumer(self._channel, self._host)
+        self._ps_consumer = PSConsumer(self._channel, self._host)
         self._services = self._get_services()
-        self._timer = self._get_timer()
 
         if callback:
             kwargs = dict(
@@ -195,106 +192,82 @@ class Service(object):
 
         except Exception as e:
             print '服务加载失败'
-            traceback.print_exc()
             return {}
 
-    def _get_timer(self):
-        try:
-            members = get_members(
-                __conf__.JOB_DIR, lambda o: hasattr(o, "__timer__"))
-            svrs = []
-            for k,v in members.items():
-                svrs.append((v.__timer__, v))
 
-            return svrs
-
-        except Exception as e:
-            print '定时任务加载失败'
-            traceback.print_exc()
-            return {}
-
-    def _subscribe(self):
-        self._consumer.subscribe()
-
+    def _queue_consumer(self):
         for i in range(self._processes):
             if not iswin:
                 if fork() > 0:
                     continue
 
             with LogicContext():
-                log_consumer = log("log/service-consumer", level = 'info' if not __conf__.DEBUG else 'debug')
+                log_queue_consumer = log("log/service-queue-consumer", level = 'info' if not __conf__.DEBUG else 'debug')
+                channel = ['REDIS_QUEUE_1', self._channel]
+                r = get_context().get_redis()
                 while True:
                     try:
-                        cmd, data = self._consumer.consume()
+                        channel, data = r.brpop(channel)
+                        cmd, data = Pack.unpack(data)
                         srv_funcs = self._services.get(cmd, ())
-
-                        #if cmd and data:
-                        #    cmd_key = '{}_{}'.format(cmd, data.get('sendtime',''))
-                        #    count = get_context().get_redis().lrem(__conf__.SERVICE_LISTKEY, cmd_key)
 
                         ps = []
                         for func in srv_funcs:
-                            #if func.__sub_mode__ == -1 and count==0:
-                            #    continue
-
                             try:
-                                #log_consumer.debug("{}".format(data))
-                                log_consumer.debug("{} -- {} -- {} -- {} -- {}".format(cmd, data.get('sensor_type', ''), data.get('dtime', ''), data.get('ts', ''), data.get('unix_time', '')))
+                                log_queue_consumer.debug("{} -- {} -- {} -- {} -- {}".format(cmd, data.get('sensor_type', ''), data.get('dtime', ''), data.get('ts', ''), data.get('unix_time', '')))
                                 func(data)
-                            except:
-                                log_consumer.error("{}".format(traceback.format_exc()))
+                            except Exception as e:
+                                log_queue_consumer.error("{}".format(traceback.format_exc()))
 
-                    except Exception as e :
-                        log_consumer.error("{}".format(traceback.format_exc()))
-                        while True:
-                            try:
-                                self._consumer.subscribe()
-                                break
-                            except:
-                                time.sleep(5)
+                    except (SystemExit, KeyboardInterrupt) as e:
+                        log_queue_consumer.error("{}".format(traceback.format_exc()))
+                        break;
+                    except Exception as e:
+                        log_queue_consumer.error("{}".format(traceback.format_exc()))
+                        time.sleep(1)
+                        continue
 
             exit(0)
 
-    def _corn(self):
-        if not iswin:
-            if fork() > 0:
-                return
+    def ps_consumer(self):
+        for i in range(self._processes):
+            if not iswin:
+                if fork() > 0:
+                    continue
 
-        s = sched.scheduler(time.time, time.sleep)
-        log_timer = log("log/service-timer",level = 'info' if not __conf__.DEBUG else 'debug')
+            with LogicContext():
+                self._ps_consumer.subscribe()
+                log_psconsumer = log("log/service-psconsumer", level = 'info' if not __conf__.DEBUG else 'debug')
+                while True:
+                    try:
+                        cmd, data = self._ps_consumer.consume()
+                        srv_funcs = self._services.get(cmd, ())
 
-        def event_func(task):
-            delay_ts, func = task
-            log_timer.debug("*START* {}-{}".format(delay_ts, func))
-            try:
-                threading.Thread(target=func).start()
-                #func()
-                log_timer.debug("*END*\r\n")
-            except:
-                log_timer.debug("{}-{}-{}".format(delay_ts, func, traceback.format_exc()))
+                        ps = []
+                        for func in srv_funcs:
+                            try:
+                                log_psconsumer.debug("{} -- {} -- {} -- {} -- {}".format(cmd, data.get('sensor_type', ''), data.get('dtime', ''), data.get('ts', ''), data.get('unix_time', '')))
+                                func(data)
+                            except Exception as e:
+                                log_psconsumer.error("{}".format(traceback.format_exc()))
 
-            s.enter(delay_ts, 1, event_func, (task,))
+                    except (SystemExit, KeyboardInterrupt) as e:
+                        log_psconsumer.error("{}".format(traceback.format_exc()))
+                        break;
+                    except Exception as e:
+                        log_psconsumer.error("{}".format(traceback.format_exc()))
+                        time.sleep(1)
+                        continue
 
-
-        with LogicContext():
-            while True:
-                for task in self._timer:
-                    s.enter(2, 1, event_func, (task,))
-
-                s.run()
-
-                log_timer.debug("TIMER RELOAD")
-
-        log_timer.debug("PROCESS TIMER EXIT")
-        exit(0)
+            exit(0)
 
     def run(self):
         if not iswin:
             self._signal()
 
         try:
-            self._subscribe()
-            self._corn()
+            self.ps_consumer()
+            self._queue_consumer()
         except RuntimeError:
             print "Is running?"
             exit(-1)
@@ -304,8 +277,8 @@ class Service(object):
             pause()
 
 
-Redis.send_pack = Pack.send_pack
 service_async = Pack.async_send_pack
+service_async_queue = Pack.async_queue
 
-__all__ = ["Consumer", "Service", "srvcmd", "srvtimer", "service_async"]
+__all__ = ["PSConsumer", "Service", "srvcmd", "srvtimer", "service_async"]
 
